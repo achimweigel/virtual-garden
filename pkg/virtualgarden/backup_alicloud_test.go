692bc25c8c63fc14118347d437d7a9b9266b1875
Robert Graeff <robert.graeff@sap.com>
2021-09-27 12:40:00 +0200

    Test backup in alicloud

--- pkg/virtualgarden/backup_alicloud_test.go
+++ pkg/virtualgarden/backup_alicloud_test.go
@@ -29,8 +29,8 @@ func TestAliCloudBackup(t *testing.T) {
 
 	listAliCloudBuckets(client)
 
-	// create bucket
-	bucketName := "dfkjhdfskhdfsdhfskduhdfkh"
+	bucketName := "testbucket-virtual-garden-5514364316"
+
 	// Create a bucket (the default storage class is Standard) and set the ACL of the bucket to public read (the default ACL is private).
 	err = client.CreateBucket(bucketName, oss.ACL(oss.ACLPrivate))
 	if err != nil {
@@ -40,9 +40,18 @@ func TestAliCloudBackup(t *testing.T) {
 
 	listAliCloudBuckets(client)
 
+	err = deleteAliCloudBucket(client, bucketName)
+	if err != nil {
+		fmt.Println("Error:", err)
+		os.Exit(-1)
+	}
+
+	listAliCloudBuckets(client)
 }
 
 func listAliCloudBuckets(client *oss.Client) {
+	fmt.Println("Listing all buckets: ")
+
 	marker := ""
 	for {
 		lsRes, err := client.ListBuckets(oss.Marker(marker))
@@ -62,4 +71,65 @@ func listAliCloudBuckets(client *oss.Client) {
 			break
 		}
 	}
+}
+
+func deleteAliCloudBucket(client *oss.Client, bucketName string) error {
+	bucket, err := client.Bucket(bucketName)
+	if err != nil {
+		fmt.Println("Error:", err)
+		return err
+	}
+
+	fmt.Printf("Deleting objects of bucket %s\n", bucketName)
+	marker := ""
+	for {
+		lsRes, err := bucket.ListObjects(oss.Marker(marker))
+		if err != nil {
+			if aerr, ok := err.(oss.ServiceError); ok {
+				switch aerr.Code {
+				case "NoSuchBucket":
+					fmt.Println("Bucket does not exist")
+					return nil
+				default:
+					return err
+				}
+			} else {
+				return err
+			}
+		}
+
+		for _, object := range lsRes.Objects {
+			fmt.Printf("Deleting object %s of bucket %s\n", object.Key, bucketName)
+
+			err = bucket.DeleteObject(object.Key)
+			if err != nil {
+				fmt.Println("Error:", err)
+				return err
+			}
+		}
+
+		if lsRes.IsTruncated {
+			marker = lsRes.NextMarker
+		} else {
+			break
+		}
+	}
+
+	fmt.Printf("Deleting bucket %s\n", bucketName)
+	err = client.DeleteBucket(bucketName)
+	if err != nil {
+		if aerr, ok := err.(oss.ServiceError); ok {
+			switch aerr.Code {
+			case "NoSuchBucket":
+				fmt.Println("Bucket does not exist")
+				return nil
+			default:
+				return err
+			}
+		} else {
+			return err
+		}
+	}
+
+	return nil
 }
